bc6970ba935fe3b0308503ff813ac3725e73d9c0
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2023-12-11 21:36:19 +0600

    11.12 js

---
+++ js/11.12-varibls/index.js
@@ -0,0 +1,121 @@
+// console.log('Hello, Akerke')
+// console.log(1+1)
+// console.log(36-35)
+// console.log(56/8)
+// console.log(267-256)
+// console.log(59*46)
+
+// js деген көп зат жасауға болады
+// джаскрипт барлық браузерде орындалады
+// console принт секілді жұмыс жасайды
+// ЛОГ ОБЬЯЗАТЕЛЬНО ЖҮРЕДІ ОНЫ МІНДЕТТІ ТҮРДЕ ЖАЗУ КЕРЕК
+
+
+// айнымалы береміз = мәнін береміз
+// let age = 18  // js та айнымалының алдына лет дегенді қоямыз
+// лет арқылы айнымалы құрамыз
+// АЙНЫМАЛЫНЫҢ 3 ҚАСИЕТІ
+// 1 АТАУЫ
+// 2 МӘНІ
+// 3 ТИПІ тип данных   int, float
+// console.log(age)
+
+// let name = "Akerke"
+// let city = "Almaty"
+// let country = "kazakhstan"
+// age = 18
+
+// console.log((name))
+// console.log(city)
+// console.log(country)
+
+// console.log(name, city, country)
+
+// Айнымалының мәнін өзгерту үшін Лет ЖАЗБАЙМЫЗ
+
+// age = 19
+ 
+// console.log(age)
+
+// name = "Alina"
+// country = "USA"
+// console.log(name, country)
+
+
+// const мәні өзгермейтін айнымалы канстантс деп атаймыз
+// егер бір өзгермейтін тип данных болса канстантка саламыз
+// Аттары бірдей айнымалылар аша алмаймыз
+
+
+// const pi = 3.14
+// console.log(pi)
+
+// pi = 4.54
+
+// console.log(pi)
+
+// const isAdult = Boolean
+
+// const name = "Akerke"
+// // тек `` осы тырнақшамен ғана жұмыс істейді
+// let sure_name = "Adikyzy"
+// console.log(`My name is ${name}`)
+// console.log(`my surename is ${sure_name}`)
+
+
+// let a = 5
+// let b = 6
+// // console.log(`${a} + ${b} = ${a+b}`)
+// console.log(`(${a} * ${b}) + (${b} / ${a}) = ${a*b + b/a}`)
+
+
+// let g = 23
+// let z = 67
+// let r = 897
+// console.log(`${g} * ${z} + ${r} = ${g * z + r}`)
+
+// let name = "Акерке"
+// // let age = "18"
+// // let city = "Алматы"
+// // console.log(`Менің атым ${name}. Мен ${age}жастамын. Мен ${city}да тұрамын`)
+// console.log(`Менің атым ${name}. Мен 786+456+358 есебін шығара аламын. Жауабы: ${786*456+348} `)
+
+
+// HW
+
+// 1
+
+// let FavoriteColor = "сары"
+// let favoriteSong =  "Ultrlance"
+// let favoriteMovie = "әлеуметтік желі"
+// let  favoriteAnimal = "балапан"
+// let favoritecity = "Ганконг"
+// let favoritefood = "Стэйк"
+// let favoritecountry = "Қазақстан"
+// console.log(`Мен нені жақсы көремін? Маған ${FavoriteColor} түс ұнайды. Менің сүйікті әуенім ${favoriteSong}. Және жақсы көретін фильм ${favoriteMovie} деп аталады. Менің сүйікті үй жануарым ${favoriteAnimal}.
+// Мен жақсы көретін және сол жаққа барғым келетән қала ${favoritecity}. Менің ойымша тамақтың барлығы өте дәмді бірақ маған ұнайды ${favoritefood}. Қандай жерде болсақта әрдайым өз отаным өзәме ыстық сол себепті сүйікті елім ${favoritecountry}.`)
+
+
+// 2 
+
+// let hasCoffee = true;
+// if (hasCoffee) {
+//     console.log("I'm drinking coffee.");
+// } else {
+//     console.log("I don't drink coffee.")
+// }
+
+// 3
+
+// gravity = 9.8
+// massa = 60
+// console.log(`gravity of a person ${gravity*massa}H`)
+
+// 4
+
+// let c = 25.5
+// let f = c * 9/5 + 32
+// console.log(f)
+
+// let temperature = 36.6
+// console.log(${temperature} * ${9.5} + ${32} = ${temperature * 9/5 + 32})
