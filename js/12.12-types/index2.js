f256661038e6938e4f9b76cc9591e7c0a4b5c71b
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2023-12-16 19:43:19 +0600

    HW

--- js/12.12-types/index2.js
+++ js/12.12-types/index2.js
@@ -85,8 +85,8 @@
 
 // MIN-MAX
 
-// console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5 САМЫЙ МИНИМАЛЬНВЙ САНДЫ ШЫҒАРАДЫ ЕҢ кіші санды
-
+//console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5 САМЫЙ МИНИМАЛЬНВЙ САНДЫ ШЫҒАРАДЫ ЕҢ кіші санды
+// console.log(Math.min(14, 32, 9, 27, 41))
 // console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20 САМЫЙ МАКСТИМАЛЬНАЙ САНДЫ ШЫҒАРАДЫ ең үлкен санды
 
 // RANDOM
@@ -127,7 +127,7 @@
 
 // 2
 
-// let num = Math.floor(Math.random() * (100-50+1)) + 50;
+// let num = Math.floor(Math.random() * 50)) + 50;
 // console.log(num)
 
 // 3
@@ -196,4 +196,158 @@
 // console.log(str.split('')) // осылай жазатын болсақ бір-бір эленттен алып шығарады. ['3', '0', ' ', 'd', 'a', 'y', 's', ' ', 'o', 'f', ' ', 'p', 'u', 't', 'h', 'o', 'n'] даже ПРОБЕЛДЫ ЭЛЕМЕНТ СИЯҚТЫ АЛАДЫ.
 
 // trim(): ПРБЕЛДЫ тексттың басында или соңында удалить етеді
-let str = ""
+// let str = "         30 days of javascript       "  
+// console.log(str)    // қалай тұр солай шығарады.
+// console.log(str.trim(" "))  // екі жағында қалған бос орындарды удалить етіп шығарады.
+
+// includes(): строкада сол сөз бар ма жоқ па соны тексереді. Егер строкада біз жазған сөз бар болса тру кайтарады, в противным случае фолс.
+// let ert = "30 days of javascript"
+// console.log(ert.includes("40")) // бізге фолс қайтарады өйткені 40 деген сан ерт ішінде жоқ. егер біз 30 жазатын болсақ тру шығады өйткені ол ерттің ішінде бар
+
+// lastIndexOf(): Ішкі бағдарламаны қабылдайды, ал егер ішкі бағдарлама жолда болса, ол ішкі бағдарламаның соңғы орнын қайтарады, егер ол болмаса, -1 қайтарады.
+// let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+// console.log(string.lastIndexOf('love')) //67
+// console.log(string.lastIndexOf('you'))  //63
+
+// indexOf егер элемент строкада бар болса бірінші строканы қайтарады ал егер ол сторокада жоқ болса -1 қайтарады.
+
+// startsWith: Сөйлем сол біз еңгізген элементтен басталса тру басталмаса фолс қайтарады
+// let string = 'Love is the best to in this world'
+// console.log(string.startsWith('Love'))   // true
+
+
+// CLASS WORK
+// LEVEL 1
+// let challenge = "30 Days Of JavaScript" //1
+// // console.log(challenge)      //2
+// console.log(challenge.length)  //3
+// console.log(challenge.toUpperCase()) //4
+// console.log(challenge.toLowerCase()) //5
+// console.log(challenge.substr(0, 2))  //6
+// console.log(challenge.substring(0,2))//7
+// console.log(challenge.includes("Script")) //8
+// console.log(challenge.split())  //9
+// console.log(challenge.split(" "))  //10
+// console.log(challenge.replace("JavaScript", "Python"))  //12
+// console.log(challenge.charAt(15))   //13
+// console.log(challenge.charCodeAt(11)) // 14
+// console.log(challenge.indexOf('a'))     //15
+// console.log(challenge.lastIndexOf('30 Days Of JavaScript')) //16
+
+//joo = 'You cannot end a sentence with because because because is a conjunction' //17
+// console.log(joo.indexOf('because'))
+// console.log(joo.lastIndexOf('because')) //18
+// console.log(joo.search('because'))  //19
+
+// let sentence =  '   30 Days Of JavaScript    '  //20
+// console.log(sentence.trim())
+
+//let sentence =  '30 Days Of JavaScript' 
+// console.log(sentence.startsWith('30'))
+// console.log(sentence.endsWith('JavaScript'))
+//console.log(sentence.match('30 Days'))
+
+// let neft = '30 Days Of '  // 24
+// console.log(neft.concat('JavaScript'))
+ 
+// let language = '30 Days of JavaScript'  //25
+// console.log(language.repeat(2))
+
+// let it = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' //11
+// console.log(it.split())
+
+
+// LEVEL2
+// 1
+// let sentence = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+// console.log(sentence)
+
+// 2
+// let quate = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+// console.log(quate)
+
+// 3
+// let num = 10
+// // let nums = 10
+// console.log(typeof 10)
+
+// 4
+// let num = parseFloat ('9.8');
+// if (num !== 10)
+// num = 10
+// console.log(num)
+// 6
+// let sentence = 'I hope this course is not full of jargon'
+// console.log(sentence.includes('jargon'))
+// 7
+// num = Math.floor(Math.random() * 101)
+// console.log(num)
+// 8
+// num = Math.floor(Math.random() * 50) + 50;
+// console.log(num)
+// 9
+// num = Match.floor(Match.random() * 256)
+// console.log(num)
+// 10
+// let sen = 'JavaScript'
+// console.log(sen[4])
+// 11
+// for (let i = 1; i <= 5; i++) {
+//     let output = `${i} `;
+//     for (let j = 1; j <= 5; j++) {
+//       if (j === 1) {
+//         output += `1 `;
+//       } else {
+//         output += `${Math.pow(i, j)} `;
+//       }
+//     }
+//     console.log(output);
+//   }
+
+// 12
+// let h = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(h.substr(31,24))
+
+// LEVEL3
+// 1
+// let sentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+// int count
+// console.log(sentence.blockCount())
+// // 2
+// let sentence = 'You cannot end a sentence with because because because is a conjunction';
+
+// let countBecause = (sentence.match(/\bbecause\b/g) || []).length;
+
+// console.log("Number of occurrences of 'because':", countBecause);
+// 3
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+// Clean the text and extract words
+const cleanedText = sentence.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+const words = cleanedText.split(" ");
+
+// Count word occurrences
+let wordCount = {};
+words.forEach(word => {
+  if (wordCount[word]) {
+    wordCount[word]++;
+  } else {
+    wordCount[word] = 1;
+  }
+});
+
+// Find the most frequent word
+let mostFrequentWord = '';
+let maxCount = 0;
+for (let word in wordCount) {
+  if (wordCount[word] > maxCount) {
+    mostFrequentWord = word;
+    maxCount = wordCount[word];
+  }
+}
+
+console.log("Cleaned Text:", cleanedText);
+console.log("Most frequent word:", mostFrequentWord, "Occurrences:", maxCount);
+
+// 4
+console.log(5000 * 12 + 10000 + 12 * 15000)
---
+++ js/14.12-pratic/index3.js
@@ -0,0 +1,10 @@
+let minNumber = Math.min(14, 32, 9, 27, 41)
+console.log(minNumber)
+
+math = Math.ceil (minNumber/745)
+console.log(math)
+
+let num = Math.floor(Math.random() * 51)
+console.log(num)
+
+console.log(num + math) 
