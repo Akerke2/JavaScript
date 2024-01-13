f3245fdabe2762672f9f36895a56328dd232a41e
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2024-01-13 16:33:31 +0600

    hw.for

--- js/8.01 - Loops/index.js
+++ js/8.01 - Loops/index.js
@@ -511,7 +511,7 @@
 // }
 
 
-// console.log(`+${telNum} ${tn} ${id}`)
+// console.log(`+7 ${telNum} ${tn} ${id}`)
 
 // // 2
 // let hsl = []
@@ -538,8 +538,542 @@
 // console.log(country)
 
 // 4
-let countries= ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
-for (i = 0; i < countries.length; i++) {
-    countries[i] = countries[i].charAt()
-}
-console.log(countries)
+// let countries= ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+// for (i = 0; i < countries.length; i++) {
+//     countries[i] = countries[i].charAt()
+// }
+// console.log(countries)
+
+// 11.01.24
+
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+// ]
+// let c = []
+// for (i = countries.length - 1 ; i >= 0; i--) {
+//     c.push(countries[i])
+// }
+// console.log(c)
+
+
+// let варилс of  арқылы обращать етеміз 
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+// ]
+// let a = []
+// for (let country of countries) {
+//     if (country[0] == ["A"]) { // ВСЕГДА ЛЕТ ОФКЕ БЕРГЕН АТТЫ ЖАЗАМЫЗ  ОЛ ӘРБІР ЭЛЕМЕНТ ҚЫЛЫП АЛАДЫ
+//         console.log(country)
+//     }
+// } 
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+// ]
+// for (let country of countries) { // ЖАНЫНА ҚАЙ МАСИВВТЕН АЛЫП ЖАТҚАНЫМЫЗДЫ ЖАЗАМЫЗ
+//     if (country.length > 6) { // ұзындығы 6 дан үлкен елдер АЛАСЫЗ. ВСЕГДА КОУНТРИ ОЛ ӘРБІР ЭЛЕМЕНТ ДЕГЕН СӨЗ
+//         console.log(country) // КАНСОЛЬҒАДА КОУНТРИ
+//     }
+// // }
+
+// let numbers = [34, 23, 78, 35, 90]
+// // let k = 0
+// // for (let nums of numbers) {
+// //     k += nums
+// // } console.log(k)
+
+// // 
+// let u = 0
+// for (let l of numbers) {
+//     if (l % 2 == 1){
+//        u += l 
+//     }
+// }console.log(u)
+
+
+
+// WHILE
+
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+// ]
+// let i = 0;
+// while (i < countries.length) {
+//     console.log(countries[i])
+//     i++;
+// }
+
+// let numbers = [34, 23, 78, 35, 90]
+// let p = 0
+// while (p < numbers.length) {
+//     if (numbers[p] % 2 == 0) {
+//     console.log(numbers[p])
+//     }
+//     p++
+// }
+
+// i = 0
+// while ( i <= 10) {
+//     console.log(i)
+//     i++
+// }i = 0
+
+// i = 0
+// while ( i <= 10) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// let i = 0
+// let p = 0
+// let numbers = [34, 23, 78, 35, 90]
+// while (i < numbers.length) {
+//     p += numbers[i]
+//     i++
+// }
+// console.log(p)
+
+
+// let numbers = [34, 23, 78, 35, 90]
+// i = 0
+// while ( i < numbers.length) {
+//     if (i % 2 == 1) {
+//       i ++
+//       continue // 
+//     }
+//     console.log(numbers[i])
+//     i++
+// }
+
+// let numbers = [34, 23, 78, 35, 90]
+// i = 0
+// while ( i < numbers.length) {
+//     if (i % 2 == 1) {
+//       i ++
+//       break // 
+//     }
+//     console.log(numbers[i])
+//     i++
+// }
+
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+// ]
+// let i = 0
+// while (0 < countries.length) {
+//     if (countries.length <= 6) {
+//     continue
+//     }
+// } console.log(countries)
+
+
+// let i = 0
+// while (i < countries.length) {
+//     if (countries[i][0] !== "A") {
+//         i++
+//         continue
+//     }
+//     console.log(countries[i])
+//     i++
+// }
+
+// for (let o of countries) {
+//     if (o == "Germany") {
+//         break
+//     }
+//     console.log(o)
+// }
+
+// LEVEL2
+// const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB'
+// ]
+// let max = webTechs[0];
+// for (let i = 0; i < webTechs.length; i++){
+//     if (webTechs[i].length <= max.length) {
+//         continue
+//     }
+//     max = webTechs[i]
+//     console.log(i)
+// }
+
+// let arr = []
+// for (let i = 0; i < webTechs.length; i++) {
+//     let nem = [webTechs[i], webTechs[i].length]
+//     arr.push(nem)
+// } console.log(arr)
+
+// 14
+// let lan =  ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+// for (let i = 0; i <= lan.length -1; i++) {
+//     console.log(lan[i])
+// }
+
+// 15
+// let fruits = ['banana', 'orange', 'mango', 'lemon']
+// // for (let o of fruits){
+// //     o = "water"
+// // }console.log(fruits[o])
+
+// fruits[0] = "water"
+// console.log(fruits)
+
+// 16
+// const fullStack = [
+//     ['HTML', 'CSS', 'JS', 'React'],
+//     ['Node', 'Express', 'MongoDB']
+// ];
+// for (let i = 0; i < fullStack.length; i++) {
+//     for (let j = 0; j < fullStack[i].length; j++) {
+//         fullStack[i][j] = fullStack[i][j].toUpperCase();
+//     }
+// }
+// console.log(fullStack);
+
+// LEVEL3
+// 1
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+// ]
+// console.log(countries)
+
+// // 2
+// let sortedCountries = countries.slice()
+// console.log(sortedCountries)
+
+// // 3
+// const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB'
+// ]
+// webTechs.sort()
+// console.log(webTechs)
+
+
+// const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+// mernStack.sort()
+// console.log(mernStack)
+
+// 4
+const countries = [
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Argentina',
+    'Armenia',
+    'Australia',
+    'Austria',
+    'Azerbaijan',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Belarus',
+    'Belgium',
+    'Belize',
+    'Benin',
+    'Bhutan',
+    'Bolivia',
+    'Bosnia and Herzegovina',
+    'Botswana',
+    'Brazil',
+    'Brunei',
+    'Bulgaria',
+    'Burkina Faso',
+    'Burundi',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cape Verde',
+    'Central African Republic',
+    'Chad',
+    'Chile',
+    'China',
+    'Colombi',
+    'Comoros',
+    'Congo (Brazzaville)',
+    'Congo',
+    'Costa Rica',
+    "Cote d'Ivoire",
+    'Croatia',
+    'Cuba',
+    'Cyprus',
+    'Czech Republic',
+    'Denmark',
+    'Djibouti',
+    'Dominica',
+    'Dominican Republic',
+    'East Timor (Timor Timur)',
+    'Ecuador',
+    'Egypt',
+    'El Salvador',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Estonia',
+    'Ethiopia',
+    'Fiji',
+    'Finland',
+    'France',
+    'Gabon',
+    'Gambia, The',
+    'Georgia',
+    'Germany',
+    'Ghana',
+    'Greece',
+    'Grenada',
+    'Guatemala',
+    'Guinea',
+    'Guinea-Bissau',
+    'Guyana',
+    'Haiti',
+    'Honduras',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Iran',
+    'Iraq',
+    'Ireland',
+    'Israel',
+    'Italy',
+    'Jamaica',
+    'Japan',
+    'Jordan',
+    'Kazakhstan',
+    'Kenya',
+    'Kiribati',
+    'Korea, North',
+    'Korea, South',
+    'Kuwait',
+    'Kyrgyzstan',
+    'Laos',
+    'Latvia',
+    'Lebanon',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Macedonia',
+    'Madagascar',
+    'Malawi',
+    'Malaysia',
+    'Maldives',
+    'Mali',
+    'Malta',
+    'Marshall Islands',
+    'Mauritania',
+    'Mauritius',
+    'Mexico',
+    'Micronesia',
+    'Moldova',
+    'Monaco',
+    'Mongolia',
+    'Morocco',
+    'Mozambique',
+    'Myanmar',
+    'Namibia',
+    'Nauru',
+    'Nepal',
+    'Netherlands',
+    'New Zealand',
+    'Nicaragua',
+    'Niger',
+    'Nigeria',
+    'Norway',
+    'Oman',
+    'Pakistan',
+    'Palau',
+    'Panama',
+    'Papua New Guinea',
+    'Paraguay',
+    'Peru',
+    'Philippines',
+    'Poland',
+    'Portugal',
+    'Qatar',
+    'Romania',
+    'Russia',
+    'Rwanda',
+    'Saint Kitts and Nevis',
+    'Saint Lucia',
+    'Saint Vincent',
+    'Samoa',
+    'San Marino',
+    'Sao Tome and Principe',
+    'Saudi Arabia',
+    'Senegal',
+    'Serbia and Montenegro',
+    'Seychelles',
+    'Sierra Leone',
+    'Singapore',
+    'Slovakia',
+    'Slovenia',
+    'Solomon Islands',
+    'Somalia',
+    'South Africa',
+    'Spain',
+    'Sri Lanka',
+    'Sudan',
+    'Suriname',
+    'Swaziland',
+    'Sweden',
+    'Switzerland',
+    'Syria',
+    'Taiwan',
+    'Tajikistan',
+    'Tanzania',
+    'Thailand',
+    'Togo',
+    'Tonga',
+    'Trinidad and Tobago',
+    'Tunisia',
+    'Turkey',
+    'Turkmenistan',
+    'Tuvalu',
+    'Uganda',
+    'Ukraine',
+    'United Arab Emirates',
+    'United Kingdom',
+    'United States',
+    'Uruguay',
+    'Uzbekistan',
+    'Vanuatu',
+    'Vatican City',
+    'Venezuela',
+    'Vietnam',
+    'Yemen',
+    'Zambia',
+    'Zimbabwe'
+]
+// let mas = []
+// for (let i = 0; i < countries.length; i++) {
+//     if (countries[i].includes("land")) {
+//         mas.push(countries[i])
+//     }
+
+// }    console.log(mas)
+
+// 5
+// let maxlenght = 0
+// let lenght = " ";
+// for (let o of countries) {
+//     if (o.length > maxlenght) {
+//         maxlenght = o.length;
+//         lenght = o;
+//     }
+// }console.log(lenght)
+
+
+// 6
+// let k = []
+// for (let i = 0; i < countries.length; i++) {
+//     if (countries[i].includes("land")) {
+//         k.push(countries[i])
+//     }
+// } console.log(k)
+
+// // 7
+// let m = []
+// for (let i = 0; i < countries.length; i++) {
+//     if (countries[i].length <= 4) {
+//         m.push(countries[i])
+//     }
+// }console.log(m)
+
+// 8
+// let two = []
+// for (let i = 0; i < countries.length; i++) {
+//     if (countries[i].length <= 5) {
+//         two.push(countries[i])
+//     }
+// }console.log(two)
+
+// 9
+let upper = []
+for (let i = 0; i < countries.length; i++) {
+    countries[i] = countries[i].toUpperCase()
+    upper.push(countries[i])
+} 
+console.log(upper)
