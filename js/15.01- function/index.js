68f987bd430890928f798698c9759966b4a0a458
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2024-01-15 17:35:08 +0600

    function

---
+++ js/15.01- function/index.js
@@ -0,0 +1,154 @@
+// // 4 
+// console.log(4 + 5)
+// console.log(42 + 45)
+// console.log(4452 + 5478)
+// console.log(254 + 255)
+
+
+// function sum() {
+//     console.log(4 + 5)
+// }
+// sum();
+// функция шақырмайынША ІСТЕМЕЙДӘ
+
+// function sumtract() {
+//     console.log(7-2)
+// }
+// sumtract();
+
+// function multiply() {
+//     console.log(456 * 596)
+// }
+// multiply() ;
+
+
+// function divide () {
+//     console.log(346 / 245)
+// }
+
+// divide();
+
+
+// function sum(num1, num2) { // кез келген санды алып қоса алу үшін АРГУМЕНТ ДЕП АТАЛАДЫ
+//     console.log(num1 + num2)
+// }
+// sum(789, 345);  
+
+// function subtract(num3, num4) {
+//     console.log(num3 - num4)
+// }
+// subtract(15, 5)
+
+// function divide(num5, num6) {
+//     console.log(num5 / num6)
+// }
+// divide(15,18)
+
+
+// function kub(num1) {
+//     console.log(num1**3)
+// } 
+// kub(2);
+
+// function kB (num4) {
+//     console.log(num4 **2)
+// }
+// kB(2)
+
+// function even(num1) {
+//     if (num1 % 2 === 0){
+//         console.log("even")
+// }   else{
+//     console.log("odd")
+// }
+// }
+// even(2)
+
+
+// function odd(num1) {
+
+//     console.log(num1 % 2 === 0 ? "even" : "odd")  //тернарный оператор ? ЖӘНЕ : АРҚЫЛЫ ШЫҒАРАМЫЗ 1 ғана консоль жазу арқылы
+// }
+// odd(3)
+
+// function degree(number, degree) {
+//     console.log(number**degree)
+// }
+// degree(4, 6)
+
+
+
+// function sumOFEven(start, end) {
+//     let o = 0
+//     for(let i = start; i < end; i++) {
+//         if (i % 2 == 0) {
+//             o += i
+//         }
+        
+//     }
+//     console.log(o)
+// }
+// sumOFEven(0, 100)
+
+// HW
+// 1
+// function fullName() {
+//     console.log("Akerke")
+// } 
+// fullName()
+
+// // 2
+// function fullName(firstName, lastName) {
+//     firstName = "Akerke"
+//     lastName = "Adikyzy"
+//     console.log(firstName, lastName)
+// }
+// fullName()
+
+// // 3
+// function addNumbers(num1, num2) {
+//     console.log(num1 + num2)
+// }
+// addNumbers(5,6)
+
+// // 4
+// function areaOfRectangle(lenght, width) {
+//     console.log(`area = ${lenght * width}`)
+// }
+// areaOfRectangle(5, 2)
+
+// 5
+// function perimeterOfRectangle(lenght, width) {
+//     console.log(2 * (lenght + width))
+// }
+// perimeterOfRectangle(2, 2)
+
+// 6
+function volumeOfRectPrism (lenght, width, height) {
+    console.log(lenght * width * height)
+}
+volumeOfRectPrism(2, 2, 2)
+
+// 7
+function areaOfCircle(p, r) {
+    console.log(p * r * r)
+}
+areaOfCircle(5, 5)
+
+// 8
+function circumOfCircle(r) {
+    console.log(2 * 3.14 * r)
+}
+circumOfCircle(5)
+
+// 9
+function density(mass, volume) {
+    console.log(mass / volume)
+}
+density(16, 2)
+
+// 10
+function speed(time, distance) {
+    console.log(distance / time)
+}
+speed(5, 5)
