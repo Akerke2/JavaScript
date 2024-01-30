70d8e78ff26ad63c6e0c3fbc2cd6139fb68d39a0
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2024-01-30 08:04:22 +0600

    dom

---
+++ js/dom-29.01.24/index.js
@@ -0,0 +1,49 @@
+// const uil = document.querySelector('p')
+// console.log(uil.textContent)
+
+// const p1 = document.querySelector('#p1')
+// const p2 = document.querySelector('#p2')
+// const p3 = document.querySelector('#p3')
+// const p4 = document.querySelector('#p4')
+
+// console.log(p1.textContent)
+// console.log(p2.textContent)
+// console.log(p3.textContent)
+// console.log(p4.textContent)
+
+// const p = document.querySelectorAll('p')
+// console.log(p)
+// for(let i of p){
+//     console.log(i.textContent)
+// }
+
+// let p1 = document.querySelectorAll('p')
+// p1[3].textContent = 'абзац4'
+
+// let p = document.querySelectorAll('p')
+// // p[3].setAttribute('class', 'title')
+// // console.log(p[3])
+// p[2].className = 'iooi'
+// console.log(p[2]);
+
+// let p = document.querySelector('#paragraph1')
+// p.style.color = 'green'
+
+// let p2 = document.querySelector('#paragraph2')
+// p2.style.background = 'aqua'
+
+// let p3 = document.querySelector('#paragraph3')
+// p3.style.fontFamily  = 'cursive'
+
+
+// let p = document.querySelectorAll('p')
+// p.forEach((element, i) => {
+//     if(i % 2 === 0){
+//         element.style.color = 'green'
+//     }else{
+//         element.style.color = 'red'
+//     }
+// })
+
+
+
